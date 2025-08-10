b2702c057943804fee4df7b49b3177be503fda69
Anand Bose <anandbose16@gmail.com>
2025-08-10 19:21:29 +0530

    Update site URL and Bluesky handle

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://anandbose.dev',
+  url: 'https://anand.codes',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -143,7 +143,7 @@ const config: Config = {
             },
             {
               label: 'Bluesky',
-              href: 'https://bsky.app/profile/anandbose.dev',
+              href: 'https://bsky.app/profile/anand.codes',
             },
             {
               label: 'Mastodon',
